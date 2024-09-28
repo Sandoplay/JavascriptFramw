6e38a274f901b892457f1d0192f8b7d150a9c1f4
Sandoplay <olesgta28@gmail.com>
2024-09-28 17:41:07 +0300

    prettier

--- Lab4/src/paginatable.ts
+++
@@ -1,6 +0,0 @@
-import { Representable } from './representable';
-
-export interface Paginatable<TEntity extends Representable> {
-  getPaginated(pageNumber: number, pageSize: number): TEntity[];
-  getTotalCount(): number;
-}
