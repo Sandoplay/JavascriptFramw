5749ee3e1f40f06a602b60e39ef6a3f4a8a8fb23
Sandoplay <olesgta28@gmail.com>
2024-12-04 21:27:15 +0200

    comment

--- Lab4/src/storage.ts
+++ Lab4/src/storage.ts
@@ -2,7 +2,7 @@ export class Storage {
   private static instance: Storage;
   private constructor() {}
 
-  save<T>(key: string, data: T): any {
+  save<T>(key: string, data: T): void {
     localStorage.setItem(key, JSON.stringify(data));
   }
 
